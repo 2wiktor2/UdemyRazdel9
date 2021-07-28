42cc8c1bdd1b276d5eaedc831ab67d49c68adce8
wiktor <wiktor@bk.ru>
2021-07-28 17:42:32 +0300

    Lesson56 init

--- app/src/main/java/com/wiktor/udemyrazdel9all/MainActivity.java
+++ app/src/main/java/com/wiktor/udemyrazdel9all/MainActivity.java
@@ -2,13 +2,35 @@ package com.wiktor.udemyrazdel9all;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.wiktor.udemyrazdel9all.lessons.Lesson56;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button buttonLesson56;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonLesson56 = findViewById(R.id.button_lesson_56);
+        buttonLesson56.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_lesson_56:
+                Intent intent = new Intent(this, Lesson56.class);
+                startActivity(intent);
+                break;
+        }
+
     }
 }
