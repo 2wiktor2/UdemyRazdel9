50fa059d1fdd240b06bed132bb27cbb3a088d188
wiktor <wiktor@bk.ru>
2021-08-06 16:58:36 +0300

    Lesson_60 done

--- app/src/main/java/com/wiktor/udemyrazdel9all/lessons/Lesson60.java
+++ app/src/main/java/com/wiktor/udemyrazdel9all/lessons/Lesson60.java
@@ -8,8 +8,10 @@ import android.icu.lang.UCharacter;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.wiktor.udemyrazdel9all.R;
 
@@ -21,7 +23,10 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.channels.AsynchronousChannelGroup;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Lesson60 extends AppCompatActivity {
 
@@ -32,6 +37,14 @@ public class Lesson60 extends AppCompatActivity {
     private Button button3;
 
     private String url = "https://www.forbes.ru/rating/50-zvezd-2011/2011";
+
+    private ArrayList<String> urls;
+    private ArrayList<String> names;
+
+    private int numberOfQuestion;
+    private int numberOfRightAnswer;
+
+    private ArrayList<Button> buttons;
 
 
     @Override
@@ -44,7 +57,15 @@ public class Lesson60 extends AppCompatActivity {
         button1 = findViewById(R.id.button1);
         button2 = findViewById(R.id.button2);
         button3 = findViewById(R.id.button3);
+        buttons = new ArrayList<>();
+        buttons.add(button0);
+        buttons.add(button1);
+        buttons.add(button2);
+        buttons.add(button3);
+        urls = new ArrayList<>();
+        names = new ArrayList<>();
         getContent();
+        playGame();
     }
 
     //Метод который получает контент
@@ -52,15 +73,91 @@ public class Lesson60 extends AppCompatActivity {
         DownloadContentTask task = new DownloadContentTask();
         try {
             String content = task.execute(url).get();
+            String start = "<div class=\"panel-pane pane-rating-content\">";
+            String finish = "<footer class=\"footer\">";
+
+            Pattern pattern = Pattern.compile(start + "(.*?)" + finish);
+            Matcher matcher = pattern.matcher(content);
+            String splitContent = "";
+            while (matcher.find()) {
+                splitContent = matcher.group(1);
+            }
+            Pattern patternImage = Pattern.compile("<img src=\"(.*?)\"");
+            Pattern patternName = Pattern.compile(" alt=\"(.*?)\"");
+
+            Matcher matcherImage = patternImage.matcher(splitContent);
+            Matcher matcherName = patternName.matcher(splitContent);
+
+            while (matcherImage.find()) {
+                urls.add(matcherImage.group(1));
+            }
+            while (matcherName.find()) {
+                names.add(matcherName.group(1));
+            }
+
+            for (String s : urls) {
+                Log.i("qwertyu", "urls = " + s);
+            }
+            for (String s : names) {
+                Log.i("qwertyu", "names = " + s);
+            }
             Log.i("qwertyu", "content из интернета = " + content);
+            Log.i("qwertyu", "content из интернета = " + content.length());
+            Log.i("qwertyu", "splitContent из интернета = " + splitContent);
+            Log.i("qwertyu", "splitContent из интернета = " + splitContent.length());
         } catch (ExecutionException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
     }
 
+    private void playGame() {
+        generateQuestion();
+        DownloadImageTask task = new DownloadImageTask();
+        try {
+            Bitmap bitmap = task.execute(urls.get(numberOfQuestion)).get();
+            if (bitmap != null) {
+                imageViewStar.setImageBitmap(bitmap);
+                //в цикле устанавливабтся номера неправильных ответов
+                for (int i = 0; i < buttons.size(); i++) {
+                    if (i == numberOfRightAnswer) {
+                        buttons.get(i).setText(names.get(numberOfQuestion));
+                    } else {
+                        int wrongAnswer = generateWrongAnswer();
+                        buttons.get(i).setText(names.get(wrongAnswer));
+                    }
+                }
+            }
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void generateQuestion() {
+//        номер вопраса должен быть от0 до длины массива
+        numberOfQuestion = (int) (Math.random() * names.size());
+        // генерируем случайный номер правильного ответа
+        numberOfRightAnswer = (int) (Math.random() * buttons.size());
+    }
+
+    //метод генерирует номер неправильного ответа
+    private int generateWrongAnswer() {
+        return (int) (Math.random() * names.size());
+    }
+
+    public void onClickAnswer(View view) {
+        Button button = (Button) view;
+        String tag = button.getTag().toString();
+        if(Integer.parseInt(tag) == numberOfRightAnswer){
+            Toast.makeText(this, "Верно!", Toast.LENGTH_SHORT).show();
+        }  else {
+            Toast.makeText(this, "Ты дебил? Это же " + names.get(numberOfQuestion), Toast.LENGTH_SHORT).show();
+        }
+        playGame();
+    }
 
     //Класс для загрузки контента
     private static class DownloadContentTask extends AsyncTask<String, Void, String> {
