d913a9267eb85434fcea64777c287ec663c43352
wiktor <wiktor@bk.ru>
2021-08-25 18:03:23 +0300

    Lesson_63_2 добавлен пример CountDownTimer-a в отдельное окно

--- app/src/main/java/com/wiktor/udemyrazdel9all/lessons/ActivityTimer.java
+++ app/src/main/java/com/wiktor/udemyrazdel9all/lessons/ActivityTimer.java
@@ -9,6 +9,8 @@ import android.widget.Toast;
 
 import com.wiktor.udemyrazdel9all.R;
 
+
+//Таймер. CountDownTimer. Из урока 63 раздел 10
 public class ActivityTimer extends AppCompatActivity {
 
     private TextView textViewTimer;
@@ -21,12 +23,15 @@ public class ActivityTimer extends AppCompatActivity {
 
         textViewTimer = findViewById(R.id.textView_timer);
 
-        CountDownTimer timer = new CountDownTimer(6000, 1000) { // принимает 1 - значение в миллисекундах(продолжительность) . 2 - через какой промежуток он будет тикать(интервал)
+        CountDownTimer timer = new CountDownTimer(15000, 1000) { // принимает 1 - значение в миллисекундах(продолжительность) . 2 - через какой промежуток он будет тикать(интервал)
             @Override
             public void onTick(long millisUntilFinished) {  // будет вызываться каждые 1000 миллисекунды (указывается вторым параметом в new CountDownTimer(60000, 1000). millisUntilFinished - сколько осталось до конца таймера
                 int seconds = (int) (millisUntilFinished / 1000); // получаем сколько осталось до конца. long нужно привести к int
                 seconds++; // увеличиваем на секунду чтобы 0 не висел целую секунду
                 textViewTimer.setText(Integer.toString(seconds)); // устанавливает в textView значение таймера
+                if (millisUntilFinished < 10000) {
+                    textViewTimer.setTextColor(getResources().getColor(android.R.color.holo_red_dark));
+                }
             }
 
             @Override
